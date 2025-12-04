27f35d8421c12d4bf817d317e7f636bb1f735486
shaikrubina2004 <shaikrubina2004@gmail.com>
2025-12-04 16:24:47 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -122,15 +122,21 @@ export default function Footer() {
               <ul className="footer-submenu">
                 <li><a href="/careers/we-work.html">We Work</a></li>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                 {/* <li><a href="/careers/openings.html">Open Positions</a></li>
                 <li><a href="/careers/submit-cv.html">Submit Your CV</a></li> */}
               {/* </ul>
             </li> */} 
 =======
+=======
+>>>>>>> Stashed changes
                 <li><a href="/careers/openings.html">Open Positions</a></li>
                 <li><a href="/careers/submit-cv.html">Submit Your CV</a></li>
               </ul>
             </li>
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
             <li><a href="/services">Services</a></li>
